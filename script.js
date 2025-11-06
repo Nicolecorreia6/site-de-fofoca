86a848e64abf8cf87733494276252ed0d9c4e68d
Nicolecorreia6 <antunes.nicole@escola.pr.gov.br>
2025-11-06 13:21:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn-leia-mais').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert("Em breve: matéria completa sobre essa fofoca! 👀");
+    });
+});
